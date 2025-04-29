8c7aa9cceace4145e1ed1f2d2d9495d6b2024497
Chefffyboii <microwavechef198@gmail.com>
2025-04-29 01:12:36 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,9 @@
 import { incrementMoves } from './script.js';
 
+// Change 'let' to 'const' because 'moves' is not reassigned
+const moves = 0;
+
 Deno.test("incrementMoves should increase move counter", () => {
-    let moves = 0;
     incrementMoves();  // This function will increase the moves by 1
     if (moves !== 1) {
         throw new Error("Move counter didn't increment correctly");
